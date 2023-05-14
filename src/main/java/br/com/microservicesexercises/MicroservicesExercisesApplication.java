3a241e3d43a4cbe977492b07ebbd6a8474f9bffc
Vitor Hugo da Silva <vitorhugo.dev1997@gmail.com>
2023-05-14 18:12:34 -0300

    initial commit
    adding files generated with spring initializr

---
+++ src/main/java/br/com/microservicesexercises/MicroservicesExercisesApplication.java
@@ -0,0 +1,13 @@
+package br.com.microservicesexercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesExercisesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroservicesExercisesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/microservicesexercises/MicroservicesExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.microservicesexercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroservicesExercisesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
